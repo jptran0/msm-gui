b23a53e7664b4ef47f87199693d1399f0e829bd5
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-09-13 20:19:28 +0000

    added forms for actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -14,4 +14,39 @@ class ActorsController < ApplicationController
       
     render({ :template => "actor_templates/show" })
   end
+
+  def create
+    a = Actor.new
+    a.name = params.fetch("query_name")
+    a.dob = params.fetch("query_dob")
+    a.bio = params.fetch("query_bio")
+    a.image = params.fetch("query_image")
+
+    a.save
+
+    redirect_to "/actors"
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    the_actor = Actor.where({id: the_id})[0]
+
+    the_actor.destroy
+
+    redirect_to "/actors"
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    the_actor = Actor.where({id: the_id})[0]
+
+    the_actor.name = params.fetch("query_name")
+    the_actor.dob = params.fetch("query_dob")
+    the_actor.bio = params.fetch("query_bio")
+    the_actor.image = params.fetch("query_image")
+
+    the_actor.save
+
+    redirect_to "/actors/#{the_id}"
+  end
 end
